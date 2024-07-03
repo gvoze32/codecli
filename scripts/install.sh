a3d5b0da06ed7c8f9c1164043a7f2a8430dcaf4c
Syafa Adena <gvoze32@gmail.com>
2024-07-04 03:39:15 +0700

    add docker

--- scripts/install.sh
+++ scripts/install.sh
@@ -141,10 +141,9 @@ case $ubuntu_version in
         install_fnm
 
         # Install dependencies
-        sudo apt install -y at git npm build-essential php php8.1-common php-gd php-mbstring php-curl php8.1-mysql php-json php8.1-xml php-fpm python3 python3-pip zip unzip dos2unix docker docker.io docker-compose
+        sudo apt install -y pythonpy apt-transport-https ca-certificates gnupg-agent software-properties-common at git npm build-essential php php8.1-common php-gd php-mbstring php-curl php8.1-mysql php-json php8.1-xml php-fpm python3 python3-pip zip unzip dos2unix docker.io docker-compose
         pip3 install requests selenium colorama bs4 wget pyfiglet
         systemctl start atd
-        sudo apt install -y pythonpy apt-transport-https ca-certificates gnupg-agent software-properties-common
 
         # Install rclone
         curl https://rclone.org/install.sh | sudo bash
@@ -177,10 +176,9 @@ case $ubuntu_version in
         install_fnm
 
         # Install dependencies
-        sudo apt install -y at git npm build-essential php7.4-cli php-gd php-mbstring php-curl php-mysqli php-json php-dom php-fpm python3 python3-pip zip unzip dos2unix docker docker.io docker-compose
+        sudo apt install -y pythonpy apt-transport-https ca-certificates gnupg-agent software-properties-common at git npm build-essential php7.4-cli php-gd php-mbstring php-curl php-mysqli php-json php-dom php-fpm python3 python3-pip zip unzip dos2unix docker.io docker-compose
         python3 -m pip install requests selenium colorama bs4 wget pyfiglet chardet urllib3
         systemctl start atd
-        sudo apt install -y pythonpy apt-transport-https ca-certificates gnupg-agent software-properties-common
 
         # Install rclone
         curl https://rclone.org/install.sh | sudo bash
@@ -208,10 +206,9 @@ case $ubuntu_version in
         install_fnm
 
         # Install dependencies
-        sudo apt install -y curl at git npm build-essential php php7.2-common php-gd php-mbstring php-curl php7.2-mysql php-json php7.2-xml php-fpm python python3-pip zip unzip dos2unix docker docker.io docker-compose
+        sudo apt install -y pythonpy apt-transport-https ca-certificates gnupg-agent software-properties-common curl at git npm build-essential php php7.2-common php-gd php-mbstring php-curl php7.2-mysql php-json php7.2-xml php-fpm python python3-pip zip unzip dos2unix docker.io docker-compose
         pip3 install requests selenium colorama bs4 wget pyfiglet
         systemctl start atd
-        sudo apt install -y pythonpy apt-transport-https ca-certificates gnupg-agent software-properties-common
 
         # Install rclone
         curl https://rclone.org/install.sh | sudo bash
@@ -246,10 +243,21 @@ case $ubuntu_version in
         install_fnm
 
         # Install dependencies
-        sudo apt install -y at git npm build-essential php8.3 libapache2-mod-php php8.3-common php8.3-cli php8.3-mbstring php8.3-bcmath php8.3-fpm php8.3-mysql php8.3-zip php8.3-gd php8.3-curl php8.3-xml python3 python3-pip zip unzip dos2unix checkinstall libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev docker docker.io docker-compose
+        sudo apt install -y pythonpy apt-transport-https ca-certificates gnupg-agent software-properties-common ca-certificates curl at git npm build-essential php8.3 libapache2-mod-php php8.3-common php8.3-cli php8.3-mbstring php8.3-bcmath php8.3-fpm php8.3-mysql php8.3-zip php8.3-gd php8.3-curl php8.3-xml python3 python3-pip zip unzip dos2unix checkinstall libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev
         pip3 install requests selenium colorama bs4 wget pyfiglet
         systemctl start atd
-        sudo apt install -y pythonpy apt-transport-https ca-certificates gnupg-agent software-properties-common
+
+        # Docker
+        sudo install -m 0755 -d /etc/apt/keyrings
+        sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+        sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+        # Add the repository to Apt sources:
+        echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        sudo apt-get update
 
         # Install rclone
         curl https://rclone.org/install.sh | sudo bash
