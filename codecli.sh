0649a77d51c7ee558385d314f209c41f021c1c96
Syafa Adena <gvoze32@gmail.com>
2024-07-04 04:30:55 +0700

    docker reinstall

--- codecli.sh
+++ codecli.sh
@@ -193,7 +193,7 @@ PORT=$port
 NAMA_PELANGGAN=$user
 PASSWORD_PELANGGAN=$pw
 EOF
-sudo docker-compose -p $user up -d
+sudo docker compose -p $user up -d
 if [ -d "/home/codeusers/$user" ]; then
 cd /home/codeusers/$user
 
@@ -226,7 +226,7 @@ MEMORY=$mem
 EOF
 sed -i '$ d' /home/codeusersmemlimit/docker-compose.yml
 echo "          memory: $mem" >> /home/codeusersmemlimit/docker-compose.yml
-sudo docker-compose -p $user up -d
+sudo docker compose -p $user up -d
 if [ -d "/home/codeusersmemlimit/$user" ]; then
 cd /home/codeusersmemlimit/$user
 
@@ -340,7 +340,7 @@ cd /home/codeusers
 cd /home/codeusersmemlimit
         ;;
 esac
-sudo docker-compose -p $user down
+sudo docker compose -p $user down
 rm -rf $user
 }
 
@@ -382,8 +382,8 @@ PASSWORD_PELANGGAN=$newpw
 EOF
     echo "Password changed successfully for user $user"
     
-    sudo docker-compose -p $user down
-    sudo docker-compose -p $user up -d
+    sudo docker compose -p $user down
+    sudo docker compose -p $user up -d
     echo "Docker container restarted for user $user"
     
     if [ "$option" = "2" ]; then
@@ -420,13 +420,13 @@ case "$response" in
     [yY][eE][sS]|[yY]) 
 at $waktu <<END
 cd /home/codeusers
-sudo docker-compose -p $user down
+sudo docker compose -p $user down
 END
         ;;
     *)
 at $waktu <<END
 cd /home/codeusersmemlimit
-sudo docker-compose -p $user down
+sudo docker compose -p $user down
 END
         ;;
 esac
--- scripts/install.sh
+++ scripts/install.sh
@@ -21,6 +21,20 @@ install_fnm() {
         npm -v
 }
 
+install_docker_app() {
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+}
+
 install_docker() {
         sudo adduser --disabled-password --gecos "" codeusers
         sudo cat > /home/codeusers/docker-compose.yml << EOF
@@ -250,6 +264,7 @@ case $ubuntu_version in
         install_docker_memlimit
         blank_env
         custom_docker_size
+        install_docker_app
         
         # Install ioncube
         install_ioncube
