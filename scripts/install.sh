292deed9b295a918e8b2df308059f9242d054a81
Syafa Adena <gvoze32@gmail.com>
2024-07-04 03:52:47 +0700

    add message

--- scripts/install.sh
+++ scripts/install.sh
@@ -255,6 +255,7 @@ case $ubuntu_version in
         sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
         sudo apt-get update
 
+        echo "Installing Docker.."
         sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
         # Install rclone
