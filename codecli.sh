c3d6b64ccb905df4db6dca9610a8f08f15c4074d
Syafa Adena <gvoze32@gmail.com>
2024-07-04 19:28:51 +0700

    apply root check

--- codecli.sh
+++ codecli.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+rootcheck() {
+    if [ "$(id -u)" != "0" ]; then
+        echo "This script must be run as root" 1>&2
+        exit 1
+    fi
+}
+
+rootcheck
+
 # COMMANDS
 
 banner() {
